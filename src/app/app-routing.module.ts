27d28f1ee9fe5aa55b37f01de0d292ba7caf7bb8
HarshK <harshshailkul@gmail.com>
2021-06-11 23:01:32 +0530

    Added Resolver
    Added Resolver to load data automatically when open edit or detail components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from "@angular/router";
 import { RecipeBookComponent } from "./recipe-book/recipe-book.component";
 import { RecipeDetailComponent } from "./recipe-book/recipe-detail/recipe-detail.component";
 import { RecipeEditComponent } from "./recipe-book/recipe-edit/recipe-edit.component";
+import { RecipesResolverService } from "./recipe-book/recipes.resolver.service";
 import { SelectRecipeComponent } from "./recipe-book/select-recipe/select-recipe.component";
 import { ShoppingListComponent } from "./shopping/shopping-list/shopping-list.component";
 
@@ -10,8 +11,8 @@ const appRoutes: Routes = [
     { path: '', redirectTo: '/recipes', pathMatch: 'full'},
     { path: 'shopping-list', component: ShoppingListComponent },
     { path: 'recipes', component: RecipeBookComponent, children: [
-        { path: 'new', component: RecipeEditComponent },
-        { path: ':id', component: RecipeDetailComponent },
+        { path: 'new', component: RecipeEditComponent, resolve: [RecipesResolverService] },
+        { path: ':id', component: RecipeDetailComponent, resolve: [RecipesResolverService] },
         { path: '', component: SelectRecipeComponent, pathMatch: 'full'},
         { path: ':id/edit', component: RecipeEditComponent }
     ] },
